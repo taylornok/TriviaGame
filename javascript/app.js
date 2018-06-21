e2334142750fb678c9d0ad89496a2e1f970d0038
taylornok <taylorowenskees@outlook.com>
2018-06-20 22:08:47 -0700

    timer functions working

---
+++ javascript/app.js
@@ -0,0 +1,71 @@
+
+console.log("timer started");
+
+var audio = new Audio("Jeopardy-theme-song.mp3");
+var intervalId;
+var number;    
+var clockRunning = false;
+var musicRunning = false
+
+
+//Just the timer functions!
+$("#start").on("click", function() {
+    //set boolean to determine if the game is already playing, avoid restarting timer or music
+    if (!clockRunning && !musicRunning) {
+        //start the game by setting number to desired, then decrease the number by 1 each second
+        function startGame() {
+            if (!clockRunning && !musicRunning) {
+                number = 5;
+                $("#countdownTimer").text(number);
+                intervalId = setInterval(decrement, 1000);
+                $("#gameStatus").empty();
+                playAudio();
+                clockRunning = true;
+            }
+
+        }
+        startGame();
+    }
+    //determine how fast the timer will count down, conditionals for ending and beginning timer
+    function decrement() {
+        clockRunning = true;
+        //  Decrease number by one.
+        number --;
+        if (number >= 0){
+            $("#countdownTimer").text(number);
+        }
+            //  Once number hits zero...
+        if (number === 0) {
+            
+            //  ...run the stop function.
+            stop();
+            audio.pause();
+            //  Alert the user that time is up.
+            $("#gameStatus").text("Time's Up!");
+            console.log("Time's Up!")
+        } 
+    }
+    
+
+});   
+
+//set audio on loop to play for the duration of the timer
+function playAudio(){
+    musicRunning = true;
+    audio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+    }, false);
+    audio.play();
+}
+//stop the clock, clear time interval, set clockrunning and musicrunning to false to aid in startup
+function stop(){
+    clearInterval(intervalId);
+    clockRunning = false;
+    musicRunning = false
+}
+
+
+//For the questions
+
+
