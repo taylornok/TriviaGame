8f0107a76413a8c090a6f683a51fb4d5cf8a0a4d
taylornok <taylorowenskees@outlook.com>
2018-07-02 15:33:23 -0700

    Logic works, bare bones

--- javascript/newapp.js
+++ javascript/newapp.js
@@ -6,7 +6,7 @@ var number;
 var clockRunning = false;
 var musicRunning = false
 
-
+$("#submit").hide()
 //Just the timer functions!
 $("#start").on("click", function() {
     //set boolean to determine if the game is already playing, avoid restarting timer or music
@@ -14,18 +14,19 @@ $("#start").on("click", function() {
         //start the game by setting number to desired, then decrease the number by 1 each second
         function startGame() {
             if (!clockRunning && !musicRunning) {
-                number = 100;
+                number = 120;
                 $("#countdownTimer").text(number);
                 intervalId = setInterval(decrement, 1000);
                 $("#gameStatus").empty();
-                playAudio();
-                displayTrivia();
+                // playAudio();
+                // displayQuiz();
                 clockRunning = true;
             }
 
         }
         startGame();
         $("#start").hide();
+        $("#submit").show();
     }
     //determine how fast the timer will count down, conditionals for ending and beginning timer
     function decrement() {
@@ -43,7 +44,6 @@ $("#start").on("click", function() {
             audio.pause();
             //  Alert the user that time is up.
             $("#gameStatus").text("Time's Up!");
-            displayResults();
             console.log("Time's Up!")
         } 
     }
@@ -70,18 +70,12 @@ function stop(){
 
 //For the questions
 //Define choices, question info, score
-var userAnswer;
-var correctAnswer = 0;
-
-var round = 0;
-var numPosAnswers = 0;
-var gameOver = false;
-
-
+var correct = 0;
+var incorrect = 0;
 
 // var quizDiv = $("#quiz");
 
-var questions = [
+var questionArray = [
     {
     question: "Select the primary color.",
     possAnswer: [
@@ -137,123 +131,13 @@ var questions = [
     ],
         questionAnswer: "Cancer"
         
-    },
-    {
-    question: "The headquarters of the United Nations is located in what city?",
-    possAnswer: [
-        "New York",
-        "Sacramento",
-        "Boston",
-        "San Francisco"
-    ],
-        questionAnswer: "New York"
-    },
-    {
-    question: "Tenochtitlan, founded in 1324, is now known as what city?",
-    possAnswer: [
-        "Mexico City",
-        "Buenos Aires",
-        "Lima",
-        "Quito"
-    ],
-        questionAnswer: "Mexico City"
-    },
-    {
-    question: "Someone born on November 11th is said to have what astrological sign?",
-    possAnswer: [
-        "Aquarius",
-        "Sagittarius",
-        "Cancer",
-        "Scorpio"
-    ],
-        questionAnswer: "Scorpio"
-    },
-    {
-    question: "The Bill of Rights contains how many of the first amendments to the United States Constitution?",
-    possAnswer: [
-        "Ten",
-        "Eight",
-        "Fourteen",
-        "One"
-        ],
-        questionAnswer: "Ten"
-    },
-    {
-    question: "What do the letters C and H stand for in C & H Sugar?",
-    possAnswer: [
-        "Caine & Harold",
-        "California & Hawaii",
-        "Case & Hopper",
-        "Change & Hold"
-        ],
-        questionAnswer: "California & Hawaii"
-    },
-
-
+    }    
 ];
 
-console.log('question array', questions);
-console.log(questions[0].question);
+var quizContainer = $("#quiz");
+var results = $("#results");
 
+function displayQuiz(){}
+function showResults(){}
 
-console.log('question array', questions);
-
-
-function displayTrivia() {
-
-    
-    console.log("inside displayTrivia function");
-    
-
-    var question = questions[round].question;
-    var answerChoices = questions[round].possAnswer;
-
-
-    $("#quiz").text(question);
-       
-        
-    $("#answerChoices").empty();
-    for (var i = 0; i < answerChoices.length; i++) {
-
-        var answerButtons = $("<button>");
-        answerButtons.text(answerChoices[i]);
-        answerButtons.attr('data-id', round);
-        answerButtons.val(answerChoices[i]);
-        $('#answerChoices').append(answerButtons);
-
-    }
-}
-
-$("#answerChoices").on("click", "button", function() {
-    console.log($(this).val());
-    
-    
-    if ($(this).val() == questions[round].questionAnswer){
-        correctAnswer ++;
-       
-    }
-    round++;
-    if (round >= questions.length){
-        gameOver = true;
-    }
-    if (gameOver){
-        displayResults();
-        audio.pause();
-    }else{
-        displayTrivia();
-        
-    }
-    
-})
-
-function displayResults(){
-    $(".quizContainer").empty();
-    var results = $("<p>")
-    results.text("You got " + correctAnswer + " out of " + questions.length + " questions right!")
-    $(".quizContainer").append(results);
-
-}
-
-
-
-
+displayQuiz();
